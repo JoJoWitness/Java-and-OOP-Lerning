3cb5970b38dbb7289a14b359e3de091f9d675b67
JoJoWitness <Jordano470@gmail.com>
2023-12-04 10:55:53 -0400

    Leaning the non primitive data types

---
+++ JavaBasics/NonPrimitiveData.java
@@ -0,0 +1,12 @@
+public class NonPrimitiveData {
+
+    public static void main(String[] args) {
+        
+        Integer num = null;
+        
+        String words = "Ryuk is my dog";
+
+        System.out.println("Integer: "+num);
+        System.out.println("String:" +words);
+    }
+}
