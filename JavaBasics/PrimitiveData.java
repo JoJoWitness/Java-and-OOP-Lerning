46d0617351c23d8ab90b3475f5b281bed3a731e7
JoJoWitness <Jordano470@gmail.com>
2023-12-04 10:51:03 -0400

    Learning the primitive data types in Java

---
+++ JavaBasics/PrimitiveData.java
@@ -0,0 +1,26 @@
+public class PrimitiveData {
+
+    public static void main(String[] args) {
+        byte bit = 8;
+        short shor = 1000;
+        int integer = 100000000;
+        long lon = 2099999999;
+
+        float decimal = 3.14f;
+        double decimalLarge  = 3.141592653589793238462643383279502;
+
+        char character = 'r';
+
+        boolean bool = true;
+
+        System.out.println("Byte: " + bit);
+        System.out.println("Short: "+ shor );
+        System.out.println("Integer: "+ integer);
+        System.out.println("Integer: "+ lon);
+        System.out.println("Float: " + decimal);
+        System.out.println("Double: " + decimalLarge);
+        System.out.println("Char: " + character);
+        System.out.println("Boolean: "+ bool);
+    }
+    
+}
