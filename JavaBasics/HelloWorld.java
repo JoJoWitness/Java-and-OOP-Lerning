a19633e60b8386e779340fef75aec7a2f5ae839b
JoJoWitness <Jordano470@gmail.com>
2023-12-04 10:23:41 -0400

    Learning how does Java commentaries work

--- JavaBasics/HelloWorld.java
+++ JavaBasics/HelloWorld.java
@@ -1,9 +1,15 @@
 package JavaBasics;
 
+
 class HelloWorld{
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        System.out.println("Hello, again!");
+        System.out.println("Hello, World!"); // This commentaries are to learn the basics of Java comments
+        System.out.println("Hello, again!"); // This is another single line commentarie
+   
+   
+        /*
+         I'm learning how to use Java and OOP with the Progamacion ATS, rest in peace, professor
+         */
     }
 }
