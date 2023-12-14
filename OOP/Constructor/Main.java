dce44a50450a31b92a8bdf47928187cc9adbe1e5
JoJoWitness <Jordano470@gmail.com>
2023-12-13 20:23:11 -0400

    Learned object overcharge

--- OOP/Constructor/Main.java
+++ OOP/Constructor/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
          Person person = new Person("Ryuk", 8);
+         Person p2 = new Person("12312323");
 
         person.showData();
+        p2.showData("12312323");
     }
    
 }
--- OOP/Constructor/Person.java
+++ OOP/Constructor/Person.java
@@ -2,18 +2,27 @@ package OOP.Constructor;
 
 public class Person {
 
-    String name;
+    String name, id;
     int age;
+
+
 
     public Person(String name, int age){
         this.name = name;
         this.age = age;
     }
 
+    public Person(String id){
+        this.id = id;
+    }
 
 
     public void showData(){
         System.out.println("Name of the person: "+name);
         System.out.println("Age of the person: "+age);
+    }
+
+    public void showData(String id){
+        System.out.println("Your ID is: "+ id);
     }
 }
