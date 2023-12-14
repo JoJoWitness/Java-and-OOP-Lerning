2d3e991000972ff406684422a636c33c5e6aa07a
JoJoWitness <Jordano470@gmail.com>
2023-12-13 20:32:41 -0400

    Package accesibility

---
+++ OOP/Package/Package2/Class3.java
@@ -0,0 +1,14 @@
+package OOP.Package.Package2;
+
+import OOP.Package.Package1.Class1;
+
+public class Class3 {
+    public static void main(String[] args) {
+        
+        Class1 object = new Class1();
+        
+        object.atribute1 = 1;
+        object.atribute2 = 2;
+        object.atribute3 = 3;
+    }
+}
