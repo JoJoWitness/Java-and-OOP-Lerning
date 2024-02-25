2c3c0906bde2f464c68a9af3d276d63109c5a088
JoJoWitness <Jordano470@gmail.com>
2024-02-24 20:21:38 -0400

    Rearranged the folder and files

--- GUI/Jframe/Window.java
+++ GUI/Jframe/Window.java
@@ -4,9 +4,12 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame{
   public Window(String title) {
-      this.setSize(500, 500);
+      setSize(700, 700);
       setDefaultCloseOperation(EXIT_ON_CLOSE);
       setTitle(title);
+      // setLocation(100, 100);
+      // setBounds(300, 300, 500, 500);
+      setLocationRelativeTo(null);
     }
   
 
