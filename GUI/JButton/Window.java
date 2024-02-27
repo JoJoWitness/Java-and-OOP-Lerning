a829c1209fcaf20e020e6cd818a06a07a934fd21
JoJoWitness <Jordano470@gmail.com>
2024-02-27 11:59:18 -0400

    Learned how does Jbutton works

--- GUI/JButton/Window.java
+++ GUI/JButton/Window.java
@@ -1,7 +1,6 @@
 package GUI.JLabel;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,9 +10,6 @@ import java.awt.color.*;
 import java.awt.*;
 
 public class Window extends JFrame{
-  
-  public JPanel panel;
-
   public Window(String title) {
       setSize(700, 700);
       setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -24,24 +20,15 @@ public class Window extends JFrame{
       // this.getContentPane().setBackground(Color.DARK_GRAY);
     
       startComponent();
-     
     }
   
     private void startComponent(){
-      
-      createPanels();
-      addLabels();
-      addButtons();
-    }
+      JPanel panel = new JPanel();
 
-    private void createPanels(){
-      panel = new JPanel();
       // panel.setLayout(null);
+
+
       this.getContentPane().add(panel);
-
-    }
-
-    private void addLabels(){
 
       JLabel label = new JLabel();
       // label.setOpaque(true);
@@ -65,12 +52,4 @@ public class Window extends JFrame{
       panel.add(leyend);
     }
 
-    private void addButtons(){
-      JButton button = new JButton("I agree that Ryuk is Not a Criminal");
-      button.setBounds(100, 100, 300, 40);
-      button.setFont(new Font("arial", Font.PLAIN, 20));
-      button.setMnemonic('a');
-      panel.add(button);
-    }
-      
 }
--- GUI/JButton/main.java
+++ GUI/JButton/main.java
@@ -4,11 +4,6 @@ public class main {
   public static void main(String[] args) {
     Window window = new Window("Ryuk is my dog");
 
-    // boolean bool = true;
-    // System.out.println(!bool);
-    
     window.setVisible(true);
-
-    
   }
 }
